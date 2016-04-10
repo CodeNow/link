797620dc0c95196afb02f4d977982cc5034eca0b
Nathan219 <Nathan219@gmail.com>
2016-04-10 15:24:35 -0700

    Changing which shorthash it uses

--- test/functional/tasks/instance-updated.js
+++ test/functional/tasks/instance-updated.js
@@ -22,6 +22,7 @@ var instanceUpdated = require('tasks/instance-updated')
 var NaviEntry = require('models/navi-entry')
 var masterInstance = require('../../mocks/master-instance')
 var slaveInstance = require('../../mocks/slave-instance')
+var isolatedSlaveInstance = require('../../mocks/isolated-slave-instance.json')
 var TaskFatalError = require('ponos').TaskFatalError
 
 describe('functional', function () {
@@ -120,6 +121,91 @@ describe('functional', function () {
       })
 
       describe('when there is a record in the database', function () {
+        var nockScope
+        beforeEach(function (done) {
+          nockScope = nock.load('test/functional/fixtures/slave-instance-nock.json')
+          done()
+        })
+
+        afterEach(function (done) {
+          nock.cleanAll()
+          done()
+        })
+
+        beforeEach(function (done) {
+          var naviEntry = new NaviEntry()
+          naviEntry.elasticUrl = 'api-staging-runnabledemo.runnable2.net'
+          naviEntry.ownerGithubId = 9487339
+          naviEntry.directUrls = {
+            asdf: {
+              branch: 'foo'
+            }
+          }
+          naviEntry.directUrls[slaveInstance.shortHash] = {
+            branch: 'asdf',
+            lastUpdated: new Date(1995, 11, 17)
+          }
+          naviEntry.save(done)
+        })
+
+        it('should add the instance data to the existing record', function (done) {
+          var job = { instance: slaveInstance, timestamp: new Date().valueOf() }
+          instanceUpdated(job)
+            .then(function () {
+              nockScope.forEach(function (nockedRequest) {
+                expect(nockedRequest.isDone()).to.equal(true)
+              })
+              NaviEntry.findOne({elasticUrl: 'api-staging-runnabledemo.runnable2.net'}, function (err, document) {
+                if (err) {
+                  return done(err)
+                }
+                expect(document.elasticUrl).to.equal('api-staging-runnabledemo.runnable2.net')
+                expect(Object.keys(document.directUrls).length).to.equal(2)
+
+                var subDocument = document.directUrls[slaveInstance.shortHash]
+                expect(subDocument.running).to.equal(true)
+                expect(subDocument.branch).to.equal(slaveInstance.contextVersion.appCodeVersions[0].branch)
+                expect(subDocument.ports).to.deep.equal({
+                  '80': '32818',
+                  '3000': '32817',
+                  '8000': '32819',
+                  '8080': '32820'
+                })
+                expect(subDocument.dependencies).to.deep.equal([
+                  { elasticUrl: 'mongodb-staging-runnabledemo.runnablecloud.com',
+                    shortHash: '2gxk81' },
+                  { elasticUrl: 'helloworld-staging-runnabledemo.runnablecloud.com',
+                    shortHash: '1jndz2' }
+                ])
+                done()
+              })
+            })
+            .catch(done)
+        })
+
+        it('should do nothing if the update is old', function (done) {
+          var job = { instance: slaveInstance, timestamp: new Date(1990, 11, 17).valueOf() }
+          instanceUpdated(job)
+            .catch(function (err) {
+              expect(err).to.be.an.instanceof(TaskFatalError)
+              expect(err.message).to.match(/old/)
+              nockScope.forEach(function (nockedRequest) {
+                expect(nockedRequest.isDone()).to.equal(true)
+              })
+              NaviEntry.findOne({elasticUrl: 'api-staging-runnabledemo.runnable2.net'}, function (err, document) {
+                if (err) {
+                  return done(err)
+                }
+                expect(Object.keys(document.directUrls).length).to.equal(2)
+                expect(document.directUrls[slaveInstance.shortHash]).to.exist()
+                done()
+              })
+            })
+        })
+      })
+
+
+      describe('when isolating a branch', function () {
         var nockScope
         beforeEach(function (done) {
           nockScope = nock.load('test/functional/fixtures/slave-instance-nock.json')
