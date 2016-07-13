8ba0871f95ff4a3c612319326fcec65cffddbc63
Anandkumar Patel <anand@runnable.com>
2016-07-13 14:34:57 -0700

    add test

--- test/unit/tasks/instance-created.js
+++ test/unit/tasks/instance-created.js
@@ -80,5 +80,15 @@ describe('tasks', function () {
         })
         .catch(done)
     })
+
+    it('should skip isolation updates', function (done) {
+      var job = { instance: instance, timestamp: new Date().valueOf(), action: 'isolation' }
+      instanceCreated(job)
+        .then(function (results) {
+          sinon.assert.notCalled(NaviEntry.handleInstanceUpdate)
+          done()
+        })
+        .catch(done)
+    })
   })
 })
