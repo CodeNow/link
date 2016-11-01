cd43e189a656ae0a402d25667b859ff9222a5881
Nathan219 <Nathan219@gmail.com>
2016-11-01 14:23:30 -0700

    Add script to remove the bad entries

---
+++ scripts/remove-extra-navi-entries.js
@@ -0,0 +1,56 @@
+'use strict'
+
+require('loadenv')({ debugName: 'link:env' })
+const Promise = require('bluebird')
+const NaviEntry = require('../lib/models/navi-entry')
+var mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGO)
+
+const DRY_RUN = !process.env.ACTUALLY_RUN
+
+NaviEntry.findAsync({})
+  .then(function (allRecords) {
+    const recordMap = {}
+    const multiples = {}
+    allRecords.forEach(record => {
+      if (recordMap[ record.elasticUrl ]) {
+        if (!multiples[ record.elasticUrl ]) {
+          multiples[ record.elasticUrl ] = []
+        }
+        multiples[ record.elasticUrl ].push(record)
+      } else {
+        recordMap[ record.elasticUrl ] = record
+      }
+    })
+    const promises = []
+    Object.keys(multiples).forEach(elasticUrl => {
+      let newestDate = 0
+      let newestRecord = null
+      multiples[ elasticUrl ].forEach(record => {
+        Object.keys(record.directUrls).forEach(directUrl => {
+          if (directUrl.lastUpdated > newestDate) {
+            newestDate = directUrl.lastUpdated
+            newestRecord = record
+          }
+        })
+      })
+      multiples[ elasticUrl ].forEach(record => {
+        if (record !== newestRecord) {
+          if (DRY_RUN) {
+            console.log('Remove navi-entry _id ' + record._id)
+            console.log(record)
+          } else {
+            promises.push(NaviEntry.removeByIdAsync(record._id))
+          }
+        }
+      })
+    })
+    return Promise.all(promises)
+  })
+  .catch(err => {
+    console.error(err)
+  })
+  .finally(() => {
+    process.exit(0)
+  })
